bdfee88e994ba5123476972a21ababd61ff9aea1
cgysir <cgysir@126.com>
2016-04-15 18:27:25 +0800

    loop path method

---
+++ tools/looppath.py
@@ -0,0 +1,27 @@
+﻿#! /bin/env python
+#author=changgy
+#date=20160415
+
+import os
+import os.path
+
+
+######################################################
+def traverseDirByListdir(path):
+    fileList=[]
+    for f in os.listdir(path):
+    	file = os.path.join(path,f)
+    	if os.path.isfile(file):
+    		fileList.append(file)
+    	    
+    return fileList
+    
+
+if __name__ == '__main__' :
+	 
+	#1. loop stl file list
+	fileList = traverseDirByListdir(r'./')	
+		
+	#2. loop file
+	for file in fileList:
+		print file   
